5cfc447b0f076efeffddda0c986e274e36a33946
Jake G <jake098877@gmail.com>
2020-05-16 18:14:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   end
 
   def execute_transaction
-        binding.pry
+        # binding.pry
     receiver.balance = @amount + receiver.balance
     sender.balance = sender.balance - @amount
   end
