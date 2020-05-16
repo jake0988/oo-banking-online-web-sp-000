f6a192bc903677609c1f476b31004886891a416e
Jake G <jake098877@gmail.com>
2020-05-16 17:36:22 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,9 @@
 class BankAccount
 attr_reader :name, :balance, :status
   def initialize
-    # @name = name
-    # @balance = 1000
-    # @status = open
+    @name = name
+    @balance = 1000
+    @status = open
   end
 
   def deposit
